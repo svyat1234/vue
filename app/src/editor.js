eb631aa69d9f5e4729ea100291335927df30db7a
svyat1234 <75384046+svyat1234@users.noreply.github.com>
2021-10-20 13:39:15 +0300

    Add files via upload

---
+++ app/src/editor.js
@@ -0,0 +1,49 @@
+const axios = require("axios");
+
+const DOMHelper = require("./dom-helper");
+
+require("./iframe-load");
+
+module.exports = class Editor {
+    constructor() {
+        this.iframe = document.querySelector("iframe");
+    }
+
+    open(page) {
+        this.currentPage = page;
+        axios
+            .get("../" + page)
+            .then((res) => DOMHelper.parseStrToDom(res.data))
+            .then(DOMHelper.wrapTextNodes)
+            .then((dom) => {
+                DOMHelper.virtualDom = dom;
+                return dom;
+            })
+            .then(DOMHelper.serializeDomToStr)
+            .then((html) => axios.post("./api/saveTempPage.php", { html }))
+            .then(() => this.iframe.load("../temp.html"))
+            .then(() => this.enableEditing())
+    }
+
+
+    enableEditing() {
+        this.iframe.contentDocument.body.querySelectorAll("text-editor").forEach((element) => {
+            element.contentEditable = "true";
+            element.addEventListener("input", () => {
+                this.onTextEdit(element);
+            })
+        })
+    }
+
+    onTextEdit(element) {
+        const id = element.getAttribute("nodeid");
+        DOMHelper.virtualDom.body.querySelector(`[nodeid="${id}"]`).innerHTML = element.innerHTML;
+    }
+
+    save() {
+        const newDom = DOMHelper.virtualDom.cloneNode(  DOMHelper.virtualDom);
+        DOMHelper.unwrapTextNodes(newDom);
+        const html = DOMHelper.serializeDomToStr(newDom);
+        axios.post("./api/savePage.php", { pageName: this.currentPage, html})
+    }
+}
--- app/src/main.js
+++ app/src/main.js
@@ -1,32 +1,46 @@
-const Vue = require("vue")
-const axios = require("axios")
+const Editor = require("./editor")
 
-new Vue({
-    el: "#app",
-    data: {
-        "pageList": [],
-        "newPageName": ""
-    }, 
-    methods: {
-        createPage() {
-            axios
-            .post("./api/createNewHtmlPage.php", {"name": this.newPageName})
-            .then(() => this.updatePageList())
-        },
-        updatePageList() {
-            axios
-            .get("./api/")
-            .then((response) => {
-                this.pageList = response.data
-            })
-        },
-        deletePage(page) {
-            axios
-                .post("./api/deletePage.php", {"name": page})
-                .then(() => this.updatePageList())
-        }
-    },
-    created() {
-        this.updatePageList();
-    },
-})
+window.editor = new Editor()
+
+window.onload = () => {
+    window.editor.open("index.html");
+}
+
+
+
+
+
+
+
+// const Vue = require("vue")
+// const axios = require("axios")
+
+// new Vue({
+//     el: "#app",
+//     data: {
+//         "pageList": [],
+//         "newPageName": ""
+//     }, 
+//     methods: {
+//         createPage() {
+//             axios
+//             .post("./api/createNewHtmlPage.php", {"name": this.newPageName})
+//             .then(() => this.updatePageList())
+//         },
+//         updatePageList() {
+//             axios
+//             .get("./api/")
+//             .then((response) => {
+//                 this.pageList = response.data
+//             })
+//         },
+//         deletePage(page) {
+//             axios
+//                 .post("./api/deletePage.php", {"name": page})
+//                 .then(() => this.updatePageList())
+//         }
+//     },
+//     created() {
+//         this.updatePageList();
+//     },
+// })
